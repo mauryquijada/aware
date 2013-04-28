d5c041aee89015b7fec615da3df5ee53d4405a43
Alex Putman <aputman@hmc.edu>
2013-04-27 21:16:22 -0700

    Added some xml stuff

--- android/Aware/src/com/aware/aware/MainActivity.java
+++ android/Aware/src/com/aware/aware/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
 
     @Override
